e015a5a40874bbe607323a463db1cab018aaa5d1
yonghuiz <yonghuiz@outlook.com>
2026-05-21 09:52:26 -0500

    update

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,14 +1,23 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+$uniboxEnv = getenv('UNIBOX_ENV');
+if ($uniboxEnv === false || $uniboxEnv === '') {
+    $uniboxEnv = isset($_SERVER['UNIBOX_ENV']) && $_SERVER['UNIBOX_ENV'] !== ''
+        ? $_SERVER['UNIBOX_ENV']
+        : 'product';
+}
+
+$isProduct = ($uniboxEnv === 'product');
+
+defined('YII_DEBUG') or define('YII_DEBUG', !$isProduct);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $isProduct ? 0 : 3);
+defined('YII_ENV') or define('YII_ENV', $isProduct ? 'prod' : 'dev');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
 
-if ($_SERVER['UNIBOX_ENV'] === 'product') {
+if ($isProduct) {
 
     $config = yii\helpers\ArrayHelper::merge(
         require(__DIR__ . '/../../common/config/main.php'),
@@ -24,6 +33,6 @@ if ($_SERVER['UNIBOX_ENV'] === 'product') {
         require(__DIR__ . '/../config/main-local.php')
     );
 }
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+
 $application = new yii\web\Application($config);
 $application->run();
