813e819797fa67f2c92bb2cba99f5312f4e8ea31
yonghuiz <yonghuiz@outlook.com>
2026-05-21 09:24:35 -0500

    update

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,14 +1,14 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3); 
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
-$_SERVER['UNIBOX_ENV'] = 'test';
-if($_SERVER['UNIBOX_ENV'] === 'product') {
+
+if ($_SERVER['UNIBOX_ENV'] === 'product') {
 
     $config = yii\helpers\ArrayHelper::merge(
         require(__DIR__ . '/../../common/config/main.php'),
@@ -24,6 +24,6 @@ if($_SERVER['UNIBOX_ENV'] === 'product') {
         require(__DIR__ . '/../config/main-local.php')
     );
 }
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 $application = new yii\web\Application($config);
 $application->run();
